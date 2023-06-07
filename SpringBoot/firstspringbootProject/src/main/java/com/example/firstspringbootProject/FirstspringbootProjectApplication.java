655df13d0aac92934956c8ffcef1f17188b55253
khaldi505 <iheb.khaldi505@gmail.com>
2023-06-06 20:59:33 -0700

    spring boot

---
+++ SpringBoot/firstspringbootProject/src/main/java/com/example/firstspringbootProject/FirstspringbootProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.firstspringbootProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstspringbootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstspringbootProjectApplication.class, args);
+	}
+
+}
---
+++ SpringBoot/firstspringbootProject/src/test/java/com/example/firstspringbootProject/FirstspringbootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firstspringbootProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstspringbootProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
