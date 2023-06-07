4c28f6c6903b9631306ac5ed68197ad6666c012f
khaldi505 <iheb.khaldi505@gmail.com>
2023-06-07 08:50:45 +0100

    logging

---
+++ Workshop_2/logging/src/main/java/com/example/logging/MyApplicationRunner.java
@@ -0,0 +1,20 @@
+package com.example.logging;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import java.util.logging.Logger;
+
+@Component
+public class MyApplicationRunner implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+        Logger logger = Logger.getLogger(MyApplicationRunner.class.getName());
+        logger.info("Application Started");
+        logger.info("Application Finished");
+        
+        int result = Integer.parseInt(args[0]) / Integer.parseInt(args[1]);
+        
+        }
+          
+    }
